10064e7f45f72394a33f7df3fd3445b8ff8689b0
mauriciopasetto <mauriciopasetto@gmail.com>
2023-09-29 16:04:00 -0300

    Versao 1 Interface

---
+++ app/src/main/java/com/example/clinicaveterinaria/CadastroUserActivity.kt
@@ -0,0 +1,11 @@
+package com.example.clinicaveterinaria
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CadastroUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_user)
+    }
+}
---
+++ app/src/main/java/com/example/clinicaveterinaria/TelaInicialActivity.kt
@@ -0,0 +1,11 @@
+package com.example.clinicaveterinaria
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TelaInicialActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_inicial_app)
+    }
+}
